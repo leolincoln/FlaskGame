37aae87297240a77a3e8f91ad50dbbd743bcdf47
Liu Liu <leoliu@u.northwestern.edu>
2015-07-28 14:54:17 -0500

    added port

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
 #!venv/bin/python
 from app import app, socketio
-socketio.run(app,host='0.0.0.0',port = 8089)
+import sys
+if len(sys.argv)>1:
+    port = int(sys.argv[1])
+else:
+    port = 5000
+socketio.run(app,host='0.0.0.0',port=port)
