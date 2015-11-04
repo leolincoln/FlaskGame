22f6d20e36c2c2b50d28c1a2b8b45957171e646a
liu liu <leoliu@dhcp-10-105-146-245.wireless.northwestern.private>
2015-11-04 15:20:51 -0600

    fixed color game

--- app/static/js/colors.js
+++ app/static/js/colors.js
@@ -1,31 +1,47 @@
 
 $(function() {
     console.log( "ready!" );
-// haha as long as he pressed some key its gonna change color. 
-$("body").focus();
-$("body").keypress(
+// haha as long as he pressed some key its gonna change color.
+	//Red blue orange yellow green
+		var colors=["rgba(255,0,0,0.5)","rgba(0,0,255,0.5)","rgba(255,165,0,0.7)","rgba(255,255,0,0.7)","rgba(0,128,0,0.5)"];
+		var colors_string = ["Red","Blue","Orange","Yellow","Green"]
+	
+		//choose which color to use.
+		var color_num = Math.floor((Math.random() * colors.length));
+
+		//Have moved the constant setting for color div into css file.
+		//$(".color").css("font-size","64").css("text-align","center");
+
+		//Choose which text to use.
+		var text_num = Math.floor((Math.random() * colors.length));
+		$(".color").css("color",colors[color_num]).text(colors_string[text_num]);
+    	$("body").focus();
+		$("body").focus();
+		$("body").keypress(
 	function(e){
 		console.log( "keypressed" );
-		if(correctKey($(".color").css("background-color"),e)){
+		//if(correctKey($(".color").css("background-color"),e)){
 		var $this = $(this);
 		var keyCounter = $this.data('keyCounter') || 0;
 		keyCounter+=1;
 		$this.data('keyCounter', keyCounter);
 		console.log("key pressed "+keyCounter+" times.");
 		
-		//Red blue orange yellow green 
+		//Red blue orange yellow green
 		var colors=["rgba(255,0,0,0.5)","rgba(0,0,255,0.5)","rgba(255,165,0,0.7)","rgba(255,255,0,0.7)","rgba(0,128,0,0.5)"];
-
-		//randomly select color to appear on screen. 
-		$(".color").css("background-color",colors[Math.floor((Math.random() * colors.length))]);
-    $("body").focus();
+		var colors_string = ["Red","Blue","Orange","Yellow","Green"]
+		//randomly select color to appear on screen
+		var color_num = Math.floor((Math.random() * colors.length));
+		var text_num = Math.floor((Math.random() * colors.length));
+		$(".color").css("color",colors[color_num]).text(colors_string[text_num]);
+    	$("body").focus();
 		if(keyCounter>10){
 			var currLocation =window.location.href;
 			var add = window.location.href.split("\/");
 			var newLocation = currLocation.substring(0,currLocation.length-add[add.length-1].length)+"chat";
 			window.location.href=newLocation;
 		}
-	}
+	//}
 		//console.log(e);
 	});
 });
